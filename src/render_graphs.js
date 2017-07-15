2bcc6ec0e585c4f5de6d8c66ba97dfbfa2d5a99a
jamesfe <james.ferrara@gmail.com>
2017-07-15 20:17:24 +0200

    Line graph refactor to new file

--- src/render_graphs.js
+++ src/render_graphs.js
@@ -2,7 +2,11 @@
 
 var authorGraph = require('./authorgraph');
 var deletedGraph = require('./deletedgraph');
+var lineGraphs = require('./line_graphs');
+
 authorGraph.renderAuthorGraph();
 deletedGraph.renderDeletedGraph();
 deletedGraph.renderDeletedPercentGraph('#deletedDonaldPercent', '../data/total_by_deleted.json', null);
 deletedGraph.doDeletedPercentGraphByWeek('#deletedDonaldPercentWeek', '../data/deleted_r_donald_1-2016_to_22-2017_by_week.json', null);
+
+// TODO: Load line graph
